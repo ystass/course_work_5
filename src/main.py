54373ac72331a0b9bebb85454ccc757bcf29656e
ystass <stepanischevyy@mail.ru>
2024-05-02 22:28:07 +0400

    changes

--- src/main.py
+++ src/main.py
@@ -3,16 +3,20 @@ from connect import HHParser
 from database import create_database
 from tables import creating_tables
 from tables import data_entry
+from dbmanager import DBManager
 def main():
     list_id = [1479944, 64174, 3530, 8550, 13819, 48735, 50233, 51296, 55125, 56433]
-    hh = HHParser(list_id)
-    #print(hh.job_employers())
-    print(hh.get_employers_list())
+    #dbname = 'courswork5'
+    #hh = HHParser(list_id)
+    ##print(hh.job_employers())
+    #print(hh.get_employers_list())
     #print(hh.filter_salary())
-    #params = config()
-    #create_database(params)
-    #creating_tables(params)
-    #data_entry(params)
+    params = config()
+    # create_database(params)
+    # creating_tables(params)
+    # data_entry(params, list_id)
+    dbm = DBManager()
+    print(dbm.get_companies_and_vacancies_count(params))
 
 
 if __name__ == '__main__':
