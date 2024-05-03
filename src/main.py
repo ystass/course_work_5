584113175560544d48eb539861b71f606b7a53e1
ystass <stepanischevyy@mail.ru>
2024-05-03 11:25:24 +0400

    decor

--- src/main.py
+++ src/main.py
@@ -6,18 +6,36 @@ from tables import data_entry
 from dbmanager import DBManager
 def main():
     list_id = [1479944, 64174, 3530, 8550, 13819, 48735, 50233, 51296, 55125, 56433]
-    #dbname = 'courswork5'
-    #hh = HHParser(list_id)
-    ##print(hh.job_employers())
-    #print(hh.get_employers_list())
-    #print(hh.filter_salary())
+    print('Подготавливаем данные для таблиц')
+    hh = HHParser(list_id)
+    hh.get_employers_list()
+    hh.filter_salary()
     params = config()
-    # create_database(params)
-    # creating_tables(params)
-    # data_entry(params, list_id)
+    print('Создаем базу данных')
+    create_database(params)
+    print('Создаем таблицы')
+    creating_tables(params)
+    print('Заполняем таблицы')
+    data_entry(params, list_id)
     dbm = DBManager()
-    print(dbm.get_companies_and_vacancies_count(params))
-
+    print('Теперь Вы можете получить')
+    choice = input('1 - список всех компаний и кол-во вакансий у каждой компании\n'
+            '2 - список всех вакансий с указанием названия компании, вакансии, зарплаты и ссылки на вакансию\n'
+            '3 - среднюю зарплату по вакансиям\n'
+            '4 - список всех вакансий, у которых зарплата выше средней по всем вакансиям\n'
+            '5 - список вакансий, в названии которых содержится ключевое слово\n')
+    if choice == '1':
+        print(dbm.get_companies_and_vacancies_count(params))
+    elif choice == '2':
+        print(dbm.get_all_vacancies(params))
+    elif choice == '3':
+        print(dbm.get_avg_salary(params))
+    elif choice == '4':
+        print(dbm.get_vacancies_with_higher_salary(params))
+    elif choice == '5':
+        keyword = str(input('Введите слово для поиска в названии вакансии(например - менеджер:\n'))
+        #keywords = 'менеджер'
+        print(dbm.get_vacancies_with_keyword(keyword, params))
 
 if __name__ == '__main__':
     main()
